c03941353d2d367405d5a526a3fbb25354622f7b
Nick Shook <nicholas.shook@gmail.com>
2014-09-08 10:35:50 -0700

    added tmuxinator and updated vim settings

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -2,8 +2,24 @@
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 
 # Install Packages
-brew install phantomjs \
+brew install \
+  autoconf \
+  automake \
   elasticsearch \
-  vim \
+  phantomjs \
+  reattach-to-user-namespace \
   the_silver_searcher \
-
+  tmux \
+  vim \
+  wget \
+  git \
+  go \
+  imagemagick \
+  leiningen \
+  libelf \
+  libevent \
+  libxml2 \
+  libxslt \
+  openssl \
+  postgresql \
+  graphiz
