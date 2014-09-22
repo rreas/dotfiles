cacb2c7e9a9462ffebcf6c32426e7800bd4b3f2f
Nicholas Shook <nshook@q-centrix.com>
2014-09-22 14:21:45 -0700

    Made small alias changes and added web tmuxinator
    In part, this commit concerns https://github.com/currica/web/pull/137
    and how we should go about updating ruby. Ideally ruby can be updated in
    only one place.

--- rake_helpers/zsh.rb
+++ rake_helpers/zsh.rb
@@ -4,6 +4,8 @@ class Zsh
       puts 'setting ZSH symlinks'
       `ln -s #{zsh_config} "$HOME/.zshrc"`
       `ln -s #{zshenv_config} "$HOME/.zshenv"`
+      # Homebrew and Chruby configuration
+      ln -s "$(brew --prefix)/Library/Contributions/brew_zsh_completion.zsh" /usr/local/share/zsh/site-functions
     end
 
     def setup
