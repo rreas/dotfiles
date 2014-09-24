7453ada993d4d4e8b773b9ffdc878e155283a257
Nicholas Shook <nshook@q-centrix.com>
2014-09-24 16:46:17 -0700

    added Dillon's git changes to the rake task
    This includes creating a new environment variable for git to be able to
    source it's templates including the pre-commit template, now found in
    ``./git/templates``

--- rake_helpers/git.rb
+++ rake_helpers/git.rb
@@ -3,6 +3,7 @@ class Git
     def set_symlinks
       `ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig`
       `ln -s ~/.dotfiles/git/gitignore ~/.gitignore`
+      # Git templates are sourced in the zshrc file
     end
 
     def pull_from_origin_master
@@ -11,7 +12,9 @@ class Git
     alias_method :pull_master, :pull_from_origin_master
 
     def pull_and_rebase_from_upstream
-      # TODO
+      `git remote add upstream https://github.com/currica/dotfiles.git`
+      `git fetch upstream --prune`
+      `git checkout master && git merge upstream/master`
     end
     alias_method :pull_upstream, :pull_and_rebase_from_upstream
   end
--- zsh/variables.sh
+++ zsh/variables.sh
@@ -4,3 +4,5 @@ export PATH=/usr/local/bin:$PATH
 export EDITOR=vim
 
 export DOCKER_HOST=tcp://$(boot2docker ip 2>/dev/null):2375
+
+export GIT_TEMPLATE_DIR='~/.dotfiles/git/templates/'
